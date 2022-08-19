d1d2788116480f5bea32d9c096a2d0f837d7181b
Josh Tomaino <josh.tomaino@gmail.com>
2022-08-19 13:19:58 -0400

    Update async-sorted-merge.js

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -4,6 +4,34 @@
 
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+
+    Promise.allSettled(logSources.map(source => source.popAsync()))
+      .then(async (logs) => {
+        while(true) {  
+          let output;
+
+          logs.forEach((logged, index) => {
+            const log = logged.value || logged;
+      
+            if (log) {
+              output = output?.log?.date < log.date
+                ? output
+                : { log, index };
+            }
+          });
+
+          if (!output) {
+            break;
+          }
+
+          logs[output.index] = await logSources[output.index].popAsync();
+
+          printer.print(output.log);
+        }
+
+      printer.done();
+      
+      resolve(console.log("Async sort complete."));
+    });
   });
 };
