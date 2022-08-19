475683113aa5605b080d8adce5026a57fbf40063
Josh Tomaino <josh.tomaino@gmail.com>
2022-08-19 13:20:17 -0400

    Update sync-sorted-merge.js

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,29 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  const logs = logSources.map(l => l.pop());
+  
+  while(true) {
+    let output;
+
+    logs.forEach((log, index) => {
+      if (log) {
+        output = output?.log?.date < log.date
+          ? output
+          : { log, index };
+      }
+    });
+
+    if (!output) {
+      break;
+    }
+
+    logs[output.index] = logSources[output.index].pop();
+
+    printer.print(output.log);
+  }
+
+  printer.done();
+
   return console.log("Sync sort complete.");
 };
